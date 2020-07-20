efbaf4e0608f37802bdfaaa7bc78efa90daf5850
Xiang <chenxiang00825@gmail.com>
2020-07-19 20:45:12 -0400

    rotate

--- src/rotate_about_y.glsl
+++ src/rotate_about_y.glsl
@@ -5,8 +5,13 @@
 mat4 rotate_about_y(float theta)
 {
   /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code 
-  return identity();
+  // Replace with your code
+    mat4 res = identity();
+    res[0][0] = cos(theta);
+    res[0][2] = -sin(theta);
+    res[2][0] = sin(theta);
+    res[2][2] = cos(theta);
+    return res;
   /////////////////////////////////////////////////////////////////////////////
 }
 
