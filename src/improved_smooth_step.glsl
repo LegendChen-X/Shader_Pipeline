5919ebef9749819509c5fc028dfdf799408c4580
Xiang <chenxiang00825@gmail.com>
2020-07-21 10:29:36 -0400

    improved_smooth_step

--- src/improved_smooth_step.glsl
+++ src/improved_smooth_step.glsl
@@ -6,17 +6,14 @@
 // Inputs:
 //   f  input value
 // Returns filtered output value
-float improved_smooth_step( float f)
+float improved_smooth_step(float f)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code 
-  return f;
-  /////////////////////////////////////////////////////////////////////////////
+    if(f<=0) return 0;
+    else if(f>=1) return 1;
+    
+    return 6 * pow(f,5) - 15 * pow(f,4) + 10 * pow(f,3);
 }
-vec3 improved_smooth_step( vec3 f)
+vec3 improved_smooth_step(vec3 f)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code 
-  return f;
-  /////////////////////////////////////////////////////////////////////////////
+    return vec3(improved_smooth_step(f.x), improved_smooth_step(f.y), improved_smooth_step(f.z));
 }
